b025bf6179741d3e35f940402a9c32aafcfa0692
alde-oli <alxunit01@gmail.com>
2026-03-21 05:01:11 +0100

    feat: clean movement logic and implement directional animation logic in the player script.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,13 +2,32 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 
-func _physics_process(delta):
-	var input_dir = Vector2.ZERO
+var last_direction: Vector2 = Vector2.DOWN
 
-	input_dir.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_dir.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+func _physics_process(_delta):
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direction = direction.normalized()
+	velocity = direction * speed
 
-	input_dir = input_dir.normalized()
+	# if direction != Vector2.ZERO:
+	# 	last_direction = direction
+	# 	play_animation("walk", direction)
+	# else:
+	# 	play_animation("idle", last_direction)
 
-	velocity = input_dir * speed
 	move_and_slide()
+
+func play_animation(anim_type: String, direction: Vector2) -> void:
+	var anim_dir: String
+	var flip: bool = false
+
+	if abs(direction.x) >= abs(direction.y):
+		anim_dir = "side"
+		flip = direction.x < 0
+	elif direction.y < 0:
+		anim_dir = "up"
+	else:
+		anim_dir = "down"
+
+	$AnimatedSprite2D.flip_h = flip
+	$AnimatedSprite2D.play(anim_type + "_" + anim_dir)
