434d45c2785af91f33b597826c703aec1bd3b8a0
RaphyStoll <raphael.ferreira@ik.me>
2026-03-21 23:50:55 +0100

    merge

--- globals/GlobalItemList.gd
+++ globals/GlobalItemList.gd
@@ -3,9 +3,9 @@ extends Node
 var items: Dictionary[String, Item] = {}
 
 func _enter_tree() -> void:
-	add_item_to_list("Skabungle", Item.ITEM_TYPE.thingy, 100, 2, "res://assets/icons/pods.png")
-	add_item_to_list("Babakiki", Item.ITEM_TYPE.bajigy, 300, 5, "res://assets/icons/time.png")
-	add_item_to_list("Kungamingu", Item.ITEM_TYPE.bomboclat, 500, 1, "res://assets/icons/value.png")
+	add_item_to_list("PODS", Item.ITEM_TYPE.ELECTRONICS, 100, 2, "res://assets/icons/pods.png")
+	add_item_to_list("TIME", Item.ITEM_TYPE.PRECIOUS, 300, 5, "res://assets/icons/time.png")
+	add_item_to_list("VALUE", Item.ITEM_TYPE.PRECIOUS, 500, 1, "res://assets/icons/value.png")
 
 func add_item_to_list(name, type, value, weight, texture):
 	items[name] = Item.new()
