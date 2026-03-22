0d7333f41a8765eb8bd2295b108e261ea3169f5a
Sai <perronalexis2002@gmail.com>
2026-03-22 07:26:49 +0100

    Bye credits

---
+++ scripts/psych_button.gd
@@ -0,0 +1,64 @@
+extends Button
+
+@export var hover_scale := 1.01
+@export var rotate_amount := 3.0
+@export var cycle_time := 0.6
+
+@export var fall_distance := 80.0
+@export var fall_rotation := 90.0
+@export var fall_time := 0.35
+
+var base_scale : Vector2
+var base_rotation : float
+var base_position : Vector2
+
+var hover_tween : Tween
+
+func _ready():
+	base_scale = scale
+	base_rotation = rotation
+	base_position = position
+	
+	pivot_offset = size / 2
+
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+	pressed.connect(_on_pressed)
+
+func _on_mouse_entered():
+	if hover_tween:
+		hover_tween.kill()
+
+	scale = base_scale * hover_scale
+
+	hover_tween = create_tween()
+	hover_tween.set_loops()
+	hover_tween.set_trans(Tween.TRANS_SINE)
+	hover_tween.set_ease(Tween.EASE_IN_OUT)
+
+	hover_tween.tween_property(self, "rotation", deg_to_rad(rotate_amount), cycle_time)
+	hover_tween.tween_property(self, "rotation", deg_to_rad(-rotate_amount), cycle_time)
+
+func _on_mouse_exited():
+	if hover_tween:
+		hover_tween.kill()
+
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "scale", base_scale, 0.1)
+	tween.tween_property(self, "rotation", base_rotation, 0.1)
+
+func _on_pressed():
+	if hover_tween:
+		hover_tween.kill()
+
+	var tween = create_tween()
+	tween.set_trans(Tween.TRANS_BACK)
+	tween.set_ease(Tween.EASE_IN)
+
+	tween.set_parallel(true)
+	tween.tween_property(self, "position:y", base_position.y + fall_distance, fall_time)
+	tween.tween_property(self, "rotation", deg_to_rad(fall_rotation), fall_time)
+	tween.tween_property(self, "scale", base_scale * 0.9, fall_time)
+
+	tween.chain().tween_callback(queue_free)
